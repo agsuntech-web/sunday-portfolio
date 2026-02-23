bc6f08d1014a6d666f13145f9345e50c533e719c
SuN tEcH <sunnypacross@gmail.com>
2026-02-23 15:53:15 -0700

    Initial commit of my portfolio

---
+++ sunday-portfolio/script.js
@@ -0,0 +1,80 @@
+console.log("Js working!")
+
+console.log("AG SuNtEcH portfolio loaded successfully!");
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+hiddenElements.forEach((el) => observer.observe(el));
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+})
+
+const navItems = document.querySelectorAll(".nav-links a");
+
+navItems.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
+
+const textArray = [
+  "Innovation Development",
+  "Web Developer",
+  "Frontend Specialist"
+];
+
+let index = 0;
+let charIndex = 0;
+let currentText = "";
+let isDeleting = false;
+
+function typeEffect() {
+  const typingElement = document.querySelector(".typing");
+
+  if (index >= textArray.length) {
+    index = 0;
+  }
+
+  currentText = textArray[index];
+
+  if (!isDeleting) {
+    typingElement.textContent = currentText.substring(0, charIndex + 1);
+    charIndex++;
+
+    if (charIndex === currentText.length) {
+      isDeleting = true;
+      setTimeout(typeEffect, 1000);
+      return;
+    }
+  } else {
+    typingElement.textContent = currentText.substring(0, charIndex - 1);
+    charIndex--;
+
+    if (charIndex === 0) {
+      isDeleting = false;
+      index++;
+    }
+  }
+
+  setTimeout(typeEffect, isDeleting ? 50 : 100);
+}
+
+document.addEventListener("DOMContentLoaded", typeEffect);
